cb76de0288c5f9d5a79ad656d5426892a296b48b
Tom <tjpalmer@tjpalmer.com>
2019-05-22 19:47:20 -0700

    Kick off https

--- fly.go
+++ fly.go
@@ -1,17 +1,91 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"log"
+	"math/big"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/shibukawa/configdir"
 )
 
 func main() {
+	// usr, err := user.Current()
+	// usr.
+	// os.UserCacheDir()
 	fmt.Printf("Hello, world!\n")
+	certFile, keyFile, err := makeCert()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", certFile)
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServeTLS(":8443", certFile, keyFile, nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi from %s", r.URL.Path[1:])
+}
+
+func makeCert() (string, string, error) {
+	dir := configdir.New("fly", "fly").QueryFolders(configdir.Global)[0]
+	certName := "ca.crt"
+	keyName := "ca.key"
+	certFile := filepath.Join(dir.Path, certName)
+	keyFile := filepath.Join(dir.Path, keyName)
+	if dir.Exists(certName) && dir.Exists(keyName) {
+		return certFile, keyFile, nil
+	}
+	// Some from https://golang.org/src/crypto/tls/generate_cert.go
+	// Some from https://fale.io/blog/2017/06/05/create-a-pki-in-golang/
+	serialNumber, err := randomSerialNumber()
+	if err != nil {
+		return "", "", err
+	}
+	ca := &x509.Certificate{
+		NotAfter:     time.Now().AddDate(1, 0, 0),
+		NotBefore:    time.Now(),
+		SerialNumber: serialNumber,
+		// Subject: pkix.Name{},
+	}
+	priv, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return "", "", err
+	}
+	pub := &priv.PublicKey
+	cert, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
+	if err != nil {
+		return "", "", err
+	}
+	// Certificate.
+	certBytes := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert})
+	err = dir.WriteFile(certName, certBytes)
+	if err != nil {
+		return "", "", err
+	}
+	// Key.
+	keyOut, err := os.OpenFile(keyFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return "", "", err
+	}
+	defer keyOut.Close()
+	pem.Encode(keyOut, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(priv),
+	})
+	return certFile, keyFile, nil
+}
+
+func randomSerialNumber() (*big.Int, error) {
+	// From https://golang.org/src/crypto/tls/generate_cert.go
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	return rand.Int(rand.Reader, serialNumberLimit)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tjpalmer/fly
+
+go 1.12
+
+require github.com/shibukawa/configdir v0.0.0-20170330084843-e180dbdc8da0
