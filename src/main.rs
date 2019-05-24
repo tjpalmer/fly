f9ac0835c5a78858fdefa4582d1b7d03402b801f
Tom <tjpalmer@tjpalmer.com>
2019-05-24 06:30:14 -0700

    Work toward ssl, but dependencies stink

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,23 @@ extern crate rouille;
 
 fn main() {
     println!("Hi there!");
+    let server = rouille::Server::new_ssl(
+        "127.0.0.1:8080",
+        move |request| {
+            router!(request,
+                (GET) (/) => {
+                    rouille::Response::text("Hi!")
+                },
+                _ => rouille::Response::empty_404()
+            )
+        },
+        include_bytes!("cert.pem"),
+        include_bytes!("key.pem"),
+    );
     rouille::start_server("127.0.0.1:8080", move |request| {
         router!(request,
             (GET) (/) => {
-                rouille::Response::text("hello world")
+                rouille::Response::text("Hi!")
             },
             _ => rouille::Response::empty_404()
         )
