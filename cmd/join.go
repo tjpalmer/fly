f4adc4171b3570c27c63642069a65dd327e082de
Tom <tjpalmer@tjpalmer.com>
2019-06-01 08:35:00 -0700

    Structify cert pair

--- cmd/join.go
+++ cmd/join.go
@@ -17,6 +17,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// CertPair represents a cert, key pair.
+type CertPair struct {
+	certFile string
+	keyFile  string
+}
+
 func init() {
 	rootCmd.AddCommand(joinCmd)
 }
@@ -33,34 +39,38 @@ func join() {
 	// usr.
 	// os.UserCacheDir()
 	fmt.Printf("Hello, world!\n")
-	certFile, keyFile, err := makeCert()
+	certPair, err := makeCert()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", certFile)
+	fmt.Printf("%s\n", certPair.certFile)
 	http.HandleFunc("/", handler)
 	// log.Fatal(http.ListenAndServe(":8080", nil))
-	log.Fatal(http.ListenAndServeTLS(":8443", certFile, keyFile, nil))
+	log.Fatal(http.ListenAndServeTLS(
+		":8443", certPair.certFile, certPair.keyFile, nil,
+	))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi from %s", r.URL.Path[1:])
 }
 
-func makeCert() (string, string, error) {
+func makeCert() (CertPair, error) {
 	dir := configdir.New("fly", "fly").QueryFolders(configdir.Global)[0]
 	certName := "cert.pem"
 	keyName := "key.pem"
-	certFile := filepath.Join(dir.Path, certName)
-	keyFile := filepath.Join(dir.Path, keyName)
+	bad := CertPair{"", ""}
+	good := CertPair{
+		filepath.Join(dir.Path, certName), filepath.Join(dir.Path, keyName),
+	}
 	if dir.Exists(certName) && dir.Exists(keyName) {
-		return certFile, keyFile, nil
+		return good, nil
 	}
 	// Some from https://golang.org/src/crypto/tls/generate_cert.go
 	// Some from https://fale.io/blog/2017/06/05/create-a-pki-in-golang/
 	serialNumber, err := randomSerialNumber()
 	if err != nil {
-		return "", "", err
+		return bad, err
 	}
 	ca := &x509.Certificate{
 		NotAfter:     time.Now().AddDate(1, 0, 0),
@@ -70,30 +80,32 @@ func makeCert() (string, string, error) {
 	}
 	priv, err := rsa.GenerateKey(rand.Reader, 4096)
 	if err != nil {
-		return "", "", err
+		return bad, err
 	}
 	pub := &priv.PublicKey
 	cert, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
 	if err != nil {
-		return "", "", err
+		return bad, err
 	}
 	// Certificate.
 	certBytes := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert})
 	err = dir.WriteFile(certName, certBytes)
 	if err != nil {
-		return "", "", err
+		return bad, err
 	}
 	// Key.
-	keyOut, err := os.OpenFile(keyFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	keyOut, err := os.OpenFile(
+		good.keyFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600,
+	)
 	if err != nil {
-		return "", "", err
+		return bad, err
 	}
 	defer keyOut.Close()
 	pem.Encode(keyOut, &pem.Block{
 		Type:  "RSA PRIVATE KEY",
 		Bytes: x509.MarshalPKCS1PrivateKey(priv),
 	})
-	return certFile, keyFile, nil
+	return good, nil
 }
 
 func randomSerialNumber() (*big.Int, error) {
